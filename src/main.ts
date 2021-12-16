faf7abcc4e0d7c11ac8258bd17289498475ae99d
feilongproject <flzs6137@gmail.com>
2021-12-16 13:54:49 +0800

    [U]优化路径选择逻辑

--- src/main.ts
+++ src/main.ts
@@ -12,39 +12,45 @@ Disallow: /
 
 async function Main(request: Request): Promise<Response> {
 
-    console.log(request.url)
+    console.log(`request url: ${request.url}`)
     const requestURL = new URL(request.url)
-    const path = (requestURL.pathname + "/").replaceAll("//", "/")
-    console.log(path)
+
+    const path = (requestURL.pathname + "/").replaceAll("//", "/").split("/")
+    const Path = [path[1], path[2], path[3]]
+
+    console.log(`\npath0: ${Path[0]}\npath1: ${Path[1]}\npath2: ${Path[2]}\npath3: ${Path[3]}`)
 
 
 
-    if (path == "/robots.txt") {
-        console.log("robot page")
-        return new Response(robot, GetHeaders("txt"))
+    switch (Path[0]) {
+        case "robots.txt":
+            console.log("robot page")
+            return new Response(robot, GetHeaders("txt"))
+        case "":
+            console.log("post page")
+            var post = requestURL.searchParams.get("page")
+            if (!post) post = "1"
+            return new Response(await GetPagePost(parseInt(post)), GetHeaders("html"))
+        case "post":
+            switch (Path[1]) {
+                case "":
+                    console.log("post page")
+                    var post = requestURL.searchParams.get("page")
+                    if (!post) post = "1"
+                    return new Response(await GetPagePost(parseInt(post)), GetHeaders("html"))
+                case "show":
+                    var postId = parseInt(Path[2])
+                    console.log(`post info page ,id: ${postId}`)
+                    if (postId) return new Response(await GetPageInfo(postId), GetHeaders("html"))
+                    else return new Response(await Get404(), GetHeaders("html"))
+                case "random":
+                    console.log("post random page")
+                    return new Response(await GetPageRandom(), GetHeaders("html"))
 
-    } else if (path == "/post/" || path == "/") {
-        console.log("post page")
-        var post = requestURL.searchParams.get("page")
-        if (!post) post = "1"
-        return new Response(await GetPagePost(parseInt(post)), GetHeaders("html"))
-
-    } else if (path == "/post/random/") {
-        console.log("post random page")
-        return new Response(await GetPageRandom(), GetHeaders("html"))
-
-    } else if (path.startsWith("/post/show/")) {
-        var postId = path.split("/")[3]
-        console.log(`post info page ,id: ${postId}`)
-        if (postId) return new Response(await GetPageInfo(parseInt(postId)), GetHeaders("html"))
-        else return new Response(await Get404(), GetHeaders("html"))
-
-    } else {
-        console.log("null page")
-        return new Response(await Get404(), GetHeaders("html"))
-
-
-
+            }
+        default:
+            console.log("null page")
+            return new Response(await Get404(), GetHeaders("html"))
     }
 
 }
